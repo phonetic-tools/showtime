16c4aabec3f9a36ed8019842aa3e8e9404c91568
Philip Bordallo <git@philipbordallo.com>
2022-04-09 18:01:21 -0400

    Update jest config

--- configs/jest.config.js
+++ configs/jest.config.js
@@ -1,15 +1,19 @@
 import { env } from './env.config.js';
 
 /** @type {import('@jest/types').Config.InitialOptions} */
-export default {
-  transform: {
-    '^.+\\.(t|j)sx?$': ['@swc/jest'],
-  },
-  rootDir: '../src/',
+const config = {
+  collectCoverageFrom: ['**/*.ts'],
   coverageReporters: [
     env.CI && 'clover',
     env.CI && 'json',
     env.CI && 'lcov',
     !env.CI ? 'text' : 'text-summary',
   ].filter(Boolean),
+  ci: env.CI,
+  transform: {
+    '^.+\\.(t|j)sx?$': ['@swc/jest'],
+  },
+  rootDir: '../src/',
 };
+
+export default config;
